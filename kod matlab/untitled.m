cfce533c8a2b4f11552a5de6f693552edff75eb6
BedzieJazdaElo <125198635+BedzieJazdaElo@users.noreply.github.com>
2023-06-05 15:11:38 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Szablon_Projektu.docx
    	modified:   Szablon_Projektu.pdf
    	new file:   _minted-fajny/2B401AFA2EC3F46526E9A21F5C5F5CB789AB44FC6FB2B0F8043621FC7B107B5F.pygtex
    	new file:   _minted-fajny/9E40615C87CCB2E221C0E16018BAB52089AB44FC6FB2B0F8043621FC7B107B5F.pygtex
    	deleted:    _minted-fajny/AD0D3247702F124C4A7787572A89948C89AB44FC6FB2B0F8043621FC7B107B5F.pygtex
    	deleted:    _minted-fajny/D265259726F2336DA35F91D89C08B65289AB44FC6FB2B0F8043621FC7B107B5F.pygtex
    	modified:   fajny.pdf
    	modified:   fajny.synctex.gz
    	modified:   fajny.tex
    	modified:   "kod matlab/Bez tytu\305\202u.png"
    	new file:   kod matlab/Im.jpg
    	new file:   kod matlab/myVectorFile1.pdf
    	new file:   kod matlab/myVectorFile10.pdf
    	new file:   kod matlab/myVectorFile11.pdf
    	new file:   kod matlab/myVectorFile12.pdf
    	new file:   kod matlab/myVectorFile13.pdf
    	new file:   kod matlab/myVectorFile14.pdf
    	new file:   kod matlab/myVectorFile15.pdf
    	new file:   kod matlab/myVectorFile16.pdf
    	new file:   kod matlab/myVectorFile17.pdf
    	new file:   kod matlab/myVectorFile18.pdf
    	new file:   kod matlab/myVectorFile19.pdf
    	new file:   kod matlab/myVectorFile2.pdf
    	new file:   kod matlab/myVectorFile20.pdf
    	new file:   kod matlab/myVectorFile21.pdf
    	new file:   kod matlab/myVectorFile22.pdf
    	new file:   kod matlab/myVectorFile23.pdf
    	new file:   kod matlab/myVectorFile24.pdf
    	new file:   kod matlab/myVectorFile25.pdf
    	new file:   kod matlab/myVectorFile26.pdf
    	new file:   kod matlab/myVectorFile27.pdf
    	new file:   kod matlab/myVectorFile28.pdf
    	new file:   kod matlab/myVectorFile29.pdf
    	new file:   kod matlab/myVectorFile3.pdf
    	new file:   kod matlab/myVectorFile30.pdf
    	new file:   kod matlab/myVectorFile31.pdf
    	new file:   kod matlab/myVectorFile4.pdf
    	new file:   kod matlab/myVectorFile5.pdf
    	new file:   kod matlab/myVectorFile6.pdf
    	new file:   kod matlab/myVectorFile7.pdf
    	new file:   kod matlab/myVectorFile8.pdf
    	new file:   kod matlab/myVectorFile9.pdf
    	modified:   kod matlab/untitled.m
    	new file:   kod matlab/untitled2.asv
    	modified:   kod matlab/untitled2.m

--- kod matlab/untitled.m
+++ kod matlab/untitled.m
@@ -1,20 +1,30 @@
+% Usunięcie wszystkich zmiennych z przestrzeni roboczej, zamknięcie wszystkich otwartych okien
 clc; close all;
 
+% Wczytanie obrazu
 Im = imread('Im.jpg');
+
+% Konwersja obrazu do skali szarości
 Im_gray = rgb2gray(Im);
+
+% Konwersja obrazu RGB do obrazu indeksowanego
 [Im_ind, cmap] = rgb2ind(Im, 2048);
 
 % a
+% Wykreślenie histogramu obrazu w skali szarości
 histogram(Im_gray);
 
+% Binaryzacja obrazu
 BW1 = im2bw(Im, 0.65); BW2 = im2bw(Im_ind, cmap, 0.65);
 BW3 = im2bw(Im_gray, 0.65);
 
-figure; imshow(BW1); title('(Im, 0.65)');
-figure; imshow(BW2); title('(Im ind, cmap, 0.65)');
-figure; imshow(BW3); title('(Im gray, 0.65)');
+% Wyświetlenie wyników binaryzacji
+figure(1); imshow(BW1); title('(Im, 0.65)');disp('exportgraphics');exportgraphics(gcf,'myVectorFile1.pdf','BackgroundColor','none','ContentType','vector')
+figure(2); imshow(BW2); title('(Im ind, cmap, 0.65)');disp('exportgraphics');exportgraphics(gcf,'myVectorFile2.pdf','BackgroundColor','none','ContentType','vector')
+figure(3); imshow(BW3); title('(Im gray, 0.65)');disp('exportgraphics');exportgraphics(gcf,'myVectorFile3.pdf','BackgroundColor','none','ContentType','vector')
 
 % b
+% Binaryzacja obrazu za pomocą różnych metod
 BIN1 = imbinarize(Im);
 BIN2 = imbinarize(Im, 'global'); BIN2_a = imbinarize(Im, 'adaptive');
 BIN3 = imbinarize(Im, 0.6);
@@ -27,26 +37,32 @@ BIN4_3 = imbinarize(Im, 'adaptive', 'Sensitivity', 0.3, ...
 BIN4_7 = imbinarize(Im, 'adaptive', 'Sensitivity', 0.7, ...
     'ForegroundPolarity', 'dark');
 
-figure; imshow(BIN1(:,:,1)); title(' domyslne dzialanie imbinarize');
-figure; imshow(BIN2(:,:,1)); title(' metoda globalna ');
-figure; imshow(BIN2_a(:,:,1)); title(' metoda adaptacyjna ');
-figure; imshow(BIN3(:,:,1)); title(' granica 0.6');
-figure; imshow(BIN4(:,:,1)); title([' metoda adaptacyjna, wspolczynnik ' ...
-    'czulosci 0.5, piksele tla ciemne  ']);
-figure; imshow(BIN4_b(:,:,1)); title([' metoda adaptacyjna, wspolczynnik ' ...
-    'czulosci 0.5, piksele tla jasne  ']);
-figure; imshow(BIN4_3(:,:,1)); title([' metoda adaptacyjna, wspolczynnik ' ...
-    'czulosci 0.3, piksele tla ciemne  ']);
-figure; imshow(BIN4_7(:,:,1)); title([' metoda adaptacyjna, wspolczynnik ' ...
-    'czulosci 0.7, piksele tla ciemne  ']);
-%%
+% Wyświetlenie wyników binaryzacji
+figure(4); imshow(BIN1(:,:,1)); title(' domyslne dzialanie imbinarize');disp('exportgraphics');exportgraphics(gcf,'myVectorFile4.pdf','BackgroundColor','none','ContentType','vector')
+figure(5); imshow(BIN2(:,:,1)); title(' metoda globalna ');disp('exportgraphics');exportgraphics(gcf,'myVectorFile5.pdf','BackgroundColor','none','ContentType','vector')
+figure(6); imshow(BIN2_a(:,:,1)); title(' metoda adaptacyjna ');disp('exportgraphics');exportgraphics(gcf,'myVectorFile6.pdf','BackgroundColor','none','ContentType','vector')
+figure(7); imshow(BIN3(:,:,1)); title(' granica 0.6');disp('exportgraphics');exportgraphics(gcf,'myVectorFile7.pdf','BackgroundColor','none','ContentType','vector')
+figure(8); imshow(BIN4(:,:,1)); title([' metoda adaptacyjna, wspolczynnik ' ...
+    'czulosci 0.5, piksele tla ciemne  ']);disp('exportgraphics');exportgraphics(gcf,'myVectorFile8.pdf','BackgroundColor','none','ContentType','vector')
+figure(9); imshow(BIN4_b(:,:,1)); title([' metoda adaptacyjna, wspolczynnik ' ...
+    'czulosci 0.5, piksele tla jasne  ']);disp('exportgraphics');exportgraphics(gcf,'myVectorFile9.pdf','BackgroundColor','none','ContentType','vector')
+figure(10); imshow(BIN4_3(:,:,1)); title([' metoda adaptacyjna, wspolczynnik ' ...
+    'czulosci 0.3, piksele tla ciemne  ']);disp('exportgraphics');exportgraphics(gcf,'myVectorFile10.pdf','BackgroundColor','none','ContentType','vector')
+figure(11); imshow(BIN4_7(:,:,1)); title([' metoda adaptacyjna, wspolczynnik ' ...
+    'czulosci 0.7, piksele tla ciemne  ']);disp('exportgraphics');exportgraphics(gcf,'myVectorFile11.pdf','BackgroundColor','none','ContentType','vector')
+
 % c
+% Wyznaczenie progu binaryzacji za pomocą metody Otsu
 level = graythresh(Im_gray); % 0.635
+
+% Binaryzacja obrazu z wykorzystaniem wyznaczonego progu
 BW_gray = imbinarize(Im_gray,level);
 
-figure; imshow(BW_gray); title('z graythresh');
+% Wyświetlenie wyniku
+figure(12); imshow(BW_gray); title('z graythresh');disp('exportgraphics');exportgraphics(gcf,'myVectorFile12.pdf','BackgroundColor','none','ContentType','vector')
 
 % d1
+% Modyfikacja obrazu w skali szarości za pomocą progowania na poziomie pikseli
 sizeim = size(Im_gray);
 Im_gray_bin = Im_gray;
 for i = 1:sizeim(1)
@@ -62,15 +78,18 @@ for i = 1:sizeim(1)
         end
     end
 end
-figure; imshow(Im_gray_bin);
-figure; histogram(Im_gray_bin); grid on
-%%
-% % d2
+
+% Wyświetlenie wyniku oraz histogramu
+figure(13); imshow(Im_gray_bin);disp('exportgraphics');exportgraphics(gcf,'myVectorFile13.pdf','BackgroundColor','none','ContentType','vector')
+figure(14); histogram(Im_gray_bin); grid on;disp('exportgraphics');exportgraphics(gcf,'myVectorFile14.pdf','BackgroundColor','none','ContentType','vector')
+
+% d2
+% Wyznaczenie histogramu oraz najczęściej występującej wartości natężenia
 [pixelCounts, grayLevels] = imhist(Im_gray);
 [~, idx] = max(pixelCounts(:));
 
+% Progowanie obrazu na podstawie najczęściej występującej wartości natężenia
 l_lim = idx - (idx * 0.3); r_lim = idx + (idx * 0.3);
-
 Im_gray_bin2 = Im_gray;
 for i = 1:sizeim(1)
     for j = 1:sizeim(2)
@@ -81,11 +100,16 @@ for i = 1:sizeim(1)
         end
     end
 end
-figure; imshow(Im_gray_bin2, [0 1]);
-figure; histogram(Im_gray_bin2); grid on
-%%
+
+% Wyświetlenie wyniku oraz histogramu
+figure(15); imshow(Im_gray_bin2, [0 1]);disp('exportgraphics');exportgraphics(gcf,'myVectorFile15.pdf','BackgroundColor','none','ContentType','vector')
+figure(16); histogram(Im_gray_bin2); grid on;disp('exportgraphics');exportgraphics(gcf,'myVectorFile16.pdf','BackgroundColor','none','ContentType','vector')
+
 % 2
+% Wyrównanie histogramu
 Im_wyr =  histeq(Im_gray);
-figure; imshow(Im_gray); title('Orginal')
-figure; imshow(Im_wyr); title('Po histeq')
-figure; histogram(Im_wyr); grid on
+
+% Wyświetlenie obrazu oryginalnego, po wyrównaniu histogramu oraz histogramu obrazu po wyrównaniu
+figure(17); imshow(Im_gray); title('Orginal');disp('exportgraphics');exportgraphics(gcf,'myVectorFile17.pdf','BackgroundColor','none','ContentType','vector')
+figure(18); imshow(Im_wyr); title('Po histeq');disp('exportgraphics');exportgraphics(gcf,'myVectorFile18.pdf','BackgroundColor','none','ContentType','vector')
+figure(19); histogram(Im_wyr); grid on;disp('exportgraphics');exportgraphics(gcf,'myVectorFile19.pdf','BackgroundColor','none','ContentType','vector')
--- kod matlab/untitled2.m
+++ kod matlab/untitled2.m
@@ -1,27 +1,29 @@
-clc, clear all, close all
+% Usuwanie wszystkich zmiennych z przestrzeni roboczej, czyszczenie konsoli i zamknięcie wszystkich otwartych okien
+clc; clear all; close all;
 
-obr1 = imread("obraz1.jpg");
-
-
+% Wczytanie obrazu
+obr1 = imread("Im.jpg");
 
 %% Operacje morfologiczne
 close all;
-obr3 = imread('obraz3.png');
+obr3 = imread('Bez tytułu.png');
 
 % Tworzenie struktur elementarnych
-prostokat = strel('rectangle',[16,16])
-diament = strel('diamond',8)
-dysk = strel('disk',8)
-oktagon = strel('octagon',9)
-figure();
-subplot(2, 2, 1),imshow(getnhood(prostokat),'InitialMagnification', 'fit'), title('prostokat');
-subplot(2, 2, 2),imshow(getnhood(diament),'InitialMagnification', 'fit');title('diament');
-subplot(2, 2, 3),imshow(getnhood(dysk),'InitialMagnification', 'fit');title('dysk');
-subplot(2, 2, 4),imshow(getnhood(oktagon),'InitialMagnification', 'fit');title('oktagon');
+prostokat = strel('rectangle',[16,16]);
+diament = strel('diamond',8);
+dysk = strel('disk',8);
+oktagon = strel('octagon',9);
 
+% Wyświetlanie struktur elementarnych
+figure(20);
+subplot(2, 2, 1), imshow(getnhood(prostokat), 'InitialMagnification', 'fit') , title('prostokat');
+subplot(2, 2, 2), imshow(getnhood(diament), 'InitialMagnification', 'fit') ;title('diament');
+subplot(2, 2, 3), imshow(getnhood(dysk), 'InitialMagnification', 'fit') ;title('dysk');
+subplot(2, 2, 4), imshow(getnhood(oktagon), 'InitialMagnification', 'fit') ;title('oktagon');
+disp('exportgraphics');exportgraphics(gcf,'myVectorFile20.pdf','BackgroundColor','none','ContentType','vector');
 %% Tworzenie operacji erozji, dylatacji, zamknięcia i otwarcia
-close all, clc;
-obr2 = imread('obraz3.png'); % Wczytanie obrazu "obr2"
+close all; clc;
+obr2 = imread('Bez tytułu.png'); % Wczytanie obrazu "obr2"
 
 % Tworzenie struktury elementarnej typu "disk" o rozmiarze 3
 se = strel('disk', 3);
@@ -39,29 +41,33 @@ zamk_obr2 = imclose(obr2, se);
 otw_obr2 = imopen(obr2, se);
 
 % Wyświetlanie obrazów i ich operacji
-
-figure(),imshow(obr2), title('Obraz "obr2"');
-figure(),imshow(ero_obr2), title('Erozja');
-figure(),imshow(dyl_obr2), title('Dylatacja');
-figure(),imshow(zamk_obr2), title('Zamknięcie');
-figure(),imshow(otw_obr2), title('Otwarcie');
+figure(21),imshow(obr2), title('Obraz "obr2"');disp('exportgraphics');exportgraphics(gcf,'myVectorFile21.pdf','BackgroundColor','none','ContentType','vector');
+figure(22),imshow(ero_obr2), title('Erozja');disp('exportgraphics');exportgraphics(gcf,'myVectorFile22.pdf','BackgroundColor','none','ContentType','vector');
+figure(23),imshow(dyl_obr2), title('Dylatacja');disp('exportgraphics');exportgraphics(gcf,'myVectorFile23.pdf','BackgroundColor','none','ContentType','vector');
+figure(24),imshow(zamk_obr2), title('Zamknięcie');disp('exportgraphics');exportgraphics(gcf,'myVectorFile24.pdf','BackgroundColor','none','ContentType','vector');
+figure(25),imshow(otw_obr2), title('Otwarcie');disp('exportgraphics');exportgraphics(gcf,'myVectorFile25.pdf','BackgroundColor','none','ContentType','vector');
 
 %% Wyznaczanie gradientu morfologicznego
+inicjaly = imcrop(obr3, [778,38, 200 , 100]);
+%778,38 - 928,146
+figure(26); imshow(inicjaly); title('Obraz oryginalny')
 
-inicjaly = imcrop(obr3, [1150, 40, 400 , 210]);
-%1152,40 - 1550,250
-figure; imshow(inicjaly); title('Obraz oryginalny')
+% Erozja
 inicjaly_e = imerode(inicjaly, dysk);
+
+% Dylatacja
 inicjaly_d = imdilate(inicjaly, dysk);
+disp('exportgraphics');exportgraphics(gcf,'myVectorFile26.pdf','BackgroundColor','none','ContentType','vector');
+% Wyświetlanie obrazów po erozji i dylatacji
+figure(27); imshow(inicjaly_d), title('dylatacja');disp('exportgraphics');exportgraphics(gcf,'myVectorFile27.pdf','BackgroundColor','none','ContentType','vector');
+figure(28); imshow(inicjaly_e), title('erozja');disp('exportgraphics');exportgraphics(gcf,'myVectorFile28.pdf','BackgroundColor','none','ContentType','vector');
 
-figure()
-imshow(inicjaly_d), title('dylatacja');
-figure()
-imshow(inicjaly_e), title('erozja')
-
+% Obliczanie gradientu morfologicznego różnymi metodami
 A = inicjaly - inicjaly_e;
 B = inicjaly_d - inicjaly;
 C = 0.5 * (inicjaly_d - inicjaly_e);
-figure; imshow(A); title(' wej - erozja ')
-figure; imshow(B); title(' dylatacja - wej ')
-figure; imshow(C); title(' dylatacja - erozja ')
+
+% Wyświetlanie gradientów morfologicznych
+figure(29); imshow(A); title(' wej - erozja ');disp('exportgraphics');exportgraphics(gcf,'myVectorFile29.pdf','BackgroundColor','none','ContentType','vector');
+figure(30); imshow(B); title(' dylatacja - wej ');disp('exportgraphics');exportgraphics(gcf,'myVectorFile30.pdf','BackgroundColor','none','ContentType','vector');
+figure(31); imshow(C); title(' dylatacja - erozja ');disp('exportgraphics');exportgraphics(gcf,'myVectorFile31.pdf','BackgroundColor','none','ContentType','vector');
